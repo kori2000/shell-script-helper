5826198161ddb417322d10332d91ef0ab122203f
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2022-10-11 16:40:38 +0200

    script update

--- getkeys.sh
+++ getkeys.sh
@@ -10,12 +10,12 @@ KFILE="/tmp/authkeys.$$"
 RC=0
 
 if [[ $# -ne 1 ]]; then
-  echo "No GitHub User given => Okay, take [$DEFAULTUSER]..." >&2
   GITHUBUSER=$DEFAULTUSER
 else
-  echo "Selected GitHub User [$1]. Loading Keys... "
   GITHUBUSER=$1
 fi
+
+echo "Selected GitHub User [$GITHUBUSER]. Loading Keys... " >&2
 
 curl -s https://github.com/${GITHUBUSER}.keys > $KFILE
 
